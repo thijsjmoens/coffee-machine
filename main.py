4e0538744133ac1a3e631620ee357a2ce637ee46
Thijs Moens <thijsmoens@email.com>
2025-01-20 14:35:35 +0100

    Changed check resources

--- main.py
+++ main.py
@@ -32,6 +32,28 @@ resources = {
 
 
 # --- The functions ---
+
+# Function to check sufficient resources
+def check_resources(coffee):
+    
+    if resources['water'] < MENU[coffee]['ingredients']['water']:
+        print("Sorry there is not enough water.")
+        return True
+
+    elif resources['coffee'] < MENU[coffee]['ingredients']['coffee']:
+        print("Sorry there are not enough coffee beans.")
+        return True
+            
+    else: 
+        return False
+    
+    # Check also for milk
+    if 'milk' in resources:   
+    
+        if resources['milk'] < MENU[coffee]['ingredients']['milk']:
+            print("Sorry there is not enough milk.")
+            return True
+
     
 # Function to process the money
 def process_money(coffee):
@@ -86,28 +108,24 @@ def make_coffee(coffee):
     
     # Step 1: Check the ingredients of the coffee    
     water = MENU[coffee]['ingredients']['water']
-    milk = MENU[coffee]['ingredients']['milk']
     coffee_beans = MENU[coffee]['ingredients']['coffee'] 
     
-    # Step 2: Check resources sufficient?
-    if resources['water'] < water:
-        print("Sorry there is not enough water.")
+    if 'milk' in coffee:
+        milk = MENU[coffee]['ingredients']['milk']
+
         
-    elif resources['milk'] < milk: 
-        print("Sorry there is not enough milk.")
-        
-    elif resources['coffee'] < coffee_beans: 
-        print("Sorry there are not enough coffee beans.")
-        
-    # Step 3: process the money for the coffee
+    # Step 2: process the money for the coffee
     process_money(coffee)
 
-    # Step 4: Make Coffee and deducte ingredients from resources
+    # Step 3: Make Coffee and deducte ingredients from resources
     resources['water'] = resources['water'] - water
-    resources['milk'] = resources['milk'] - milk    
     resources['coffee'] = resources['coffee'] - coffee_beans
+    
+    if 'milk' in coffee:
+        resources['milk'] = resources['milk'] - milk    
+
         
-    # Step 5: Print if coffee is ready
+    # Step 4: Print if coffee is ready
     print(f"Here's your {coffee} ☕ Enjoy!")
     
 
@@ -141,24 +159,30 @@ check_valid_inpput = True
 # Check if the input of the user is valid
 # The input can be coffee (espresso/latte/cappuccino) or report or off
 while check_valid_inpput:
-
+    
     # Ask the user for coffee with the prompt: What would you like? (espresso/latte/cappuccino):'
-    ask_user = input('What would you like? ')
+    input_user = input('What would you like? ')
         
     # If the user want coffee
-    if ask_user == 'espresso' or ask_user == 'latte' or ask_user == 'cappuccino':
+    if input_user == 'espresso' or input_user == 'latte' or input_user == 'cappuccino':
+        
+        # Check resources
+        while check_resources(input_user):
+            
+            # Ask the user for coffee with the prompt: What would you like? (espresso/latte/cappuccino):'
+            input_user = input('What would you like? ')
         
         # Make the coffee
-        make_coffee(ask_user)
+        make_coffee(input_user)
         
     # If the user wants a report
-    elif ask_user == 'report':
+    elif input_user == 'report':
         
         # Print the report
         print_report()
         
     # If the user want to shut off this machine
-    elif ask_user == 'off':
+    elif input_user == 'off':
         
         # Shut down the machine
         exit()
@@ -168,68 +192,3 @@ while check_valid_inpput:
         
         # Set the check to false
         check_valid_inpput = False
-        
-    
-    
-    # The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
-    
-# 2. Turn off the Coffee Machine by entering “ off ” to the prompt.
-
-
-# 3. Print report.
-
-    # When the user enters “report” to the prompt, a report should be generated that shows
-    # the current resource values. e.g.
-    # Water: 100ml
-    # Milk: 50ml
-    # Coffee: 76g
-    # Money: $2.5
-    
-    
-# 4. Check resources sufficient?
-
-    # When the user chooses a drink, the program should check if there are enough resources to make that drink.
-
-    # if not enough resource, print: “ Sorry there is not enough water. ” and don't make the coffee
-    # The same should happen if another resource is depleted, e.g. milk or coffee.
-    
-    
-# 5. Process coins
-    
-    # If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
-
-    # Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-    
-    # Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2 pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-    
-    
-# 6. Check transaction successful?
-
-    # Check that the user has inserted enough money to purchase the drink they selected. 
-    # E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say “ Sorry that's not enough money. Money refunded. ”.
-
-    # But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-    # Water: 100ml
-    # Milk: 50ml
-    # Coffee: 76g
-    # Money: $2.5
-
-    # If the user has inserted too much money, the machine should offer change.  “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
-    
-    
-# 7. Make Coffee.
-
-    # If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the
-    # coffee machine resources.
-    # E.g. report before purchasing latte:
-    # Water: 300ml
-    # Milk: 200ml
-    # Coffee: 100g
-    # Money: $0
-    # Report after purchasing latte:
-    # Water: 100ml
-    # Milk: 50ml
-    # Coffee: 76g
-    # Money: $2.5
-        # b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.
-    
